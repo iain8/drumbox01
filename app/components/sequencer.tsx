04e24d895bc1d4b9595dcbf07fc24a70eb56ed40
iain8 <iain@fastmail.com>
2017-10-29 22:40:28 -0400

    sequencer parts

--- app/components/sequencer.tsx
+++ app/components/sequencer.tsx
@@ -1,9 +1,40 @@
 import { Component, h } from 'preact';
+import data from '../data/default';
 
 export default class Sequencer extends Component<any, any> {
+  constructor() {
+    super();
+
+    this.state = {
+      patternLength: 16,
+    };
+  }
+
   public render() {
     return (
-      <div id='sequencer' style='margin-left: 20px'></div>
+      <div class='sequencer'>
+        {
+          data.map((channel) => {
+            return (
+              <ul class="sequence">
+                {
+                  Array(this.state.patternLength).fill(1).map((x, i) =>
+                    <li class={ `beat ${channel.pattern.charAt(i) === '1' ? 'on' : ''}` } />
+                  )
+                }
+                <li><a href="#" class="clear-sequence"></a></li>
+              </ul>
+            );
+          })
+        }
+        <ul class="sequence indicator">
+          {
+            Array(this.state.patternLength).fill(1).map((x, i) =>
+              <li class={ `beat ${i === 0 ? 'on' : ''}` } />
+            )
+          }
+        </ul>
+      </div>
     );
   }
 }
