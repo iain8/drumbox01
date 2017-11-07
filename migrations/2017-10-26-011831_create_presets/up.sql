b0bd851d86e1acb088453d8bcf330ac2a65d4a4a
iain8 <iain@fastmail.com>
2017-11-06 19:48:34 -0500

    add more data to preset

--- migrations/2017-10-26-011831_create_presets/up.sql
+++ migrations/2017-10-26-011831_create_presets/up.sql
@@ -1,5 +1,8 @@
 CREATE TABLE presets (
-  id integer PRIMARY KEY NOT NULL
+  id integer PRIMARY KEY NOT NULL,
+  tempo real NOT NULL,
+  division integer NOT NULL,
+  master_volume integer NOT NULL
 );
 
 INSERT INTO presets (id) VALUES (1);
