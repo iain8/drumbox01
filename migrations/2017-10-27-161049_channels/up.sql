13e60f3b662a0bb392fca4607179395261b8fe5d
iain8 <iain@fastmail.com>
2017-11-09 20:39:01 -0500

    better data

--- migrations/2017-10-27-161049_channels/up.sql
+++ migrations/2017-10-27-161049_channels/up.sql
@@ -8,15 +8,15 @@ CREATE TABLE channels (
 INSERT INTO channels (name, options, preset_id)
 VALUES (
   'kick',
-  '{"frequency":105,"oscAmpAttack":0,"oscAmpDecay":0.630,"oscPitchAttack":0,"oscPitchDecay":0.380,"noiseLevel":0,"oscLevel":1.0,"level":0.8}',
+  '{"frequency":105,"oscAmpAttack":0,"oscAmpDecay":0.630,"oscPitchAttack":0,"oscPitchDecay":0.380,"noiseLevel":0,"oscLevel":1.0,"level":0.8,"wave":"sine"}',
   1
 ), (
   'snare',
-  '{"frequency":800,"noiseLevel":0.35,"noiseAttack":0,"noiseDecay":0.37,"oscLevel":0,"level":0.8}',
+  '{"frequency":800,"noiseLevel":0.35,"noiseAttack":0,"noiseDecay":0.37,"oscLevel":0,"level":0.8,"wave":"sine"}',
   1
 ), (
   'hat',
-  '{"frequency":1500,"noiseLevel":0.3,"oscLevel":0,"noiseAttack":0,"noiseDecay":0.15,"channelFilterFreq":15000,"channelFilterGain":10}',
+  '{"frequency":1500,"noiseLevel":0.3,"oscLevel":0,"noiseAttack":0,"noiseDecay":0.15,"channelFilterFreq":15000,"channelFilterGain":10,"wave":"sine"}',
   1
 ), (
   'tom',
